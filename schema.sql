96a969c638ec54b820e5a2b0d80c22c13a6a941c
marcoscassian <161905696+marcoscassian@users.noreply.github.com>
2025-08-04 19:02:29 -0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,9 +1,16 @@
-drop table if exists jogo;
+drop table if exists usuarios;
+drop table if exists projetos;
 
-#tenha calma ja ajeito aq
-create table jogo (
-    id integer primary key autoincrement,
-    nomepersonagem text not null,
-    jogoorigem text not null,
-    habilidade text not null
+create table banco (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    senha TEXT NOT NULL
 );
+
+create table projetos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT NOT NULL,
+    descricao TEXT NOT NULL,
+    usuario TEXT NOT NULL
+)
